629a459aba6975adbc61af9f176ff9477bca5c38
salimkhandev <salimt450s@gmail.com>
2025-10-11 19:15:57 +0500

    improved

--- src/components/testimonials/TestimonialList.tsx
+++ src/components/testimonials/TestimonialList.tsx
@@ -177,7 +177,7 @@ function Html5VideoWithMute({ url, videoId }: { url: string; videoId: string }) 
         onClick={() => setMuted((m) => !m)}
         className="absolute bottom-2 right-2 rounded-md bg-black/60 text-white p-1.5 hover:bg-black/70"
       >
-        {muted ? <RiVolumeMuteFill size={12} /> : <RiVolumeUpFill size={14} />}
+        {muted ? <RiVolumeMuteFill size={12} /> : <RiVolumeUpFill size={12} />}
       </button>
     </div>
   );
